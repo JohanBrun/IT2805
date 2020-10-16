4360e0c623d47b06fbc74d03a3d56ed2a52c0e2f
Johan Brun <johanbrun@Johans-MacBook-Air.local>
2020-10-16 17:57:39 +0200

    Finished assignment 7

--- Assignment 7/todo.js
+++ Assignment 7/todo.js
@@ -3,20 +3,44 @@ tasks = []
 addTask = () => {
   const taskName = document.getElementById('todo-name').value;
   if (!!taskName) {
+    const li = document.createElement('li')
+
+    // Generate checkbox
     const checkbox = document.createElement('input');
-    checkbox.setAttribute('style', 'id: ')
+    const date = new Date();
+    const id = date.getTime();
+    checkbox.setAttribute('id', id);
+    checkbox.setAttribute('type', 'checkbox')
+    li.appendChild(checkbox);
+
+    // Generate label
     const label = document.createElement('label');
-    label.setAttribute('for', taskName)
-    li.innerText = taskName;
+    label.setAttribute('for', id);
+    label.innerText = taskName;
+    li.appendChild(label);
+
+    // Add list element to DOM
     document.getElementById('todo-list').appendChild(li);
     
-    const date = new Date()
+    // Store task
     const task = {
       text: taskName,
-      timestamp: date.getTime()
+      timestamp: id,
     }
     tasks.push(task)
-    console.log(tasks)
   }
+}
+
+updateOutput = () => {
+  console.log(tasks)
+  let checked = 0;
+  tasks.forEach(task => {
+    const taskElement = document.getElementById(task.timestamp)
+    if (taskElement.checked) {
+      checked++;
+    }
+  });
+  const status = document.getElementById('status');
+  status.innerText = checked + '/' + tasks.length + ' completed';
 }
 
